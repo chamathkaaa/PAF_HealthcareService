5f9fdc14508371163021b9f30fa9e8b7cb8050bb
Prabha97 <prabhashwari.lk@gmail.com>
2020-04-08 18:28:00 +0530

    Completed insertHospitals() method inside the com java class

--- Hospital_Service/src/com/Hospital_Service.java
+++ Hospital_Service/src/com/Hospital_Service.java
@@ -24,4 +24,20 @@ public class Hospital_Service {
 	public String readHospitals()  {   
 		return hospitalObj.readHospitals();
 	}
+	
+	@POST 
+	@Path("/") 
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED) 
+	@Produces(MediaType.TEXT_PLAIN) 
+	public String insertHospitals(@FormParam("Hospital_Name") String Hospital_Name,
+							@FormParam("Hospital_Address") String Hospital_Address,    
+							@FormParam("Hospital_City") String Hospital_City,       
+							@FormParam("Hospital_Phone") String Hospital_Phone,
+							@FormParam("Hospital_Email") String Hospital_Email,       
+							@FormParam("Hospital_Description") String Hospital_Description,
+							@FormParam("Open_Hours") String Open_Hours) 
+	{  
+		String output = hospitalObj.insertHospitals(Hospital_Name, Hospital_Address, Hospital_City, Hospital_Phone, Hospital_Email, Hospital_Description, Open_Hours);  
+		return output; 
+	}
 }
