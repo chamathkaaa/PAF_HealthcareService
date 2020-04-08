7d458b5c1dae7bc6101473489d3a5bbb81c713be
Prabha97 <prabhashwari.lk@gmail.com>
2020-04-08 18:59:16 +0530

    Added delete operation into com class

--- Hospital_Service/src/com/Hospital_Service.java
+++ Hospital_Service/src/com/Hospital_Service.java
@@ -63,4 +63,20 @@ public class Hospital_Service {
 		 
 		return output; 
 	}
+	
+	@DELETE 
+	@Path("/") 
+	@Consumes(MediaType.APPLICATION_XML) 
+	@Produces(MediaType.TEXT_PLAIN) 
+	public String deleteHospitals(String HospitalsData) {  
+		//Convert the input string to an XML document  
+		Document doc = Jsoup.parse(HospitalsData, "", Parser.xmlParser());     
+		
+		//Read the value from the element <itemID>  
+		String Hospital_ID = doc.select("Hospital_ID").text(); 
+		 
+		 String output = hospitalObj.deleteHospitals(Hospital_ID); 
+		 
+		 return output; 
+		 }
 }
