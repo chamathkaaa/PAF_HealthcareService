73af75170d00acc5c4b39c9005fffcf41931976c
Prabha97 <prabhashwari.lk@gmail.com>
2020-04-08 18:07:16 +0530

    Connected to the database

--- Hospital_Service/src/model/Hospital.java
+++ Hospital_Service/src/model/Hospital.java
@@ -1,5 +1,25 @@
 package model;
 
-public class Hospital {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class Hospital {
+	//A common method to connect to the DB 
+	private Connection connect() {
+		Connection con = null;
+		
+		try {
+			 Class.forName("com.mysql.jdbc.Driver");
+			 //Provide the correct details: DBServer/DBName, username, password 
+			 con= DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/healthcaredb", "root", "");
+
+			//For testing          
+			 System.out.print("Successfully connected");
+			 
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return con; 
+	}
 }
