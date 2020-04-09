8278d56d9596d9f3f755f246d2e63c8181cdf1cb
SaumyaBandara0722 <saumya19970722@gmail.com>
2020-04-09 10:14:51 +0530

    crud operations created

--- Online_Appointment/src/com/AppointmentReservation.java
+++ Online_Appointment/src/com/AppointmentReservation.java
@@ -1,6 +1,9 @@
 package com;
 import model.Appointment;
 
+import java.sql.Date;
+import java.sql.Time;
+
 //For REST Service
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -21,6 +24,58 @@ public class AppointmentReservation {
 	@Path("/")
 	@Produces(MediaType.TEXT_HTML)
 	public String readItems() {
-		return  appObj.readItems();
+		return  appObj.readDetails();
+	}
+	
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String insertDetails(@FormParam("patientId") int PatientID,
+	 @FormParam("appointmentSpec") String AppointmentSpecification,
+	 @FormParam("doctorName") String DoctorName,
+	 @FormParam("hospitalName") String HospitalName,
+	 @FormParam("dueDate") Date DueDate,
+	 @FormParam("dueTime") Time DueTime,
+	 @FormParam("status") String Status)
+	{
+	 String output = appObj.insertDetails(PatientID, AppointmentSpecification, DoctorName, HospitalName, DueDate, DueTime, Status);
+	return output;
+	}
+	
+	@PUT
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String updateDetails(String details)
+	{
+	//Convert the input string to a JSON object
+	 JsonObject detailsObject = new JsonParser().parse(details).getAsJsonObject();
+	//Read the values from the JSON object
+	 String AppointmentID = detailsObject.get("AppointmentID").getAsString();
+	 String PatientID = detailsObject.get("PatientID").getAsString();
+	 String AppointmentSpecification = detailsObject.get("AppointmentSpecification").getAsString();
+	 String DoctorName = detailsObject.get("DoctorName").getAsString();
+	 String HospitalName = detailsObject.get("HospitalName").getAsString();
+	 String DueDate = detailsObject.get("DueDate").getAsString();
+	 String DueTime = detailsObject.get("DueTime").getAsString();
+	 String Status = detailsObject.get("Status").getAsString();
+	 String output = appObj.updateDetails(AppointmentID, PatientID, AppointmentSpecification, DoctorName, HospitalName, DueDate, DueTime, Status);
+	return output;
+	}
+
+	@DELETE
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_XML)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String deleteDetails(String details)
+	{
+	//Convert the input string to an XML document
+	 Document doc = Jsoup.parse(details, "", Parser.xmlParser());
+
+	//Read the value from the element <itemID>
+	 String AppointmentID = doc.select("appointmentId").text();
+	 String output = appObj.deleteDetails(AppointmentID);
+	return output;
 	}
 }
--- Online_Appointment/src/model/Appointment.java
+++ Online_Appointment/src/model/Appointment.java
@@ -21,7 +21,41 @@ public class Appointment {
 		return con;
 	}
 	
-	public String readItems() {
+	//method to insert data
+	public String insertDetails(int PatientID, String AppointmentSpecification, String DoctorName, String HospitalName, Date DueDate, Time DueTime, String Status) {
+		String output = "";
+		try {
+			Connection con = connect();
+			if (con == null) {
+				return "Error while connecting to the database";
+			}
+			// create a prepared statement
+			String query = " insert into appointment_doctor(`appointmentId`,`patientId`,`appointmentSpec`,`doctorName`,`hospitalName`,`dueDate`,`dueTime`,`status`)"
+					+ " values (?, ?, ?, ?, ?, ?, ?, ?)";
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+			// binding values
+			preparedStmt.setInt(1, 0);
+			preparedStmt.setInt(2, PatientID);
+			preparedStmt.setString(3, AppointmentSpecification);
+			preparedStmt.setString(4, DoctorName);
+			preparedStmt.setString(5, HospitalName);
+			preparedStmt.setDate(6, DueDate);
+			preparedStmt.setTime(7, DueTime);
+			preparedStmt.setString(8, Status);
+			
+			// execute the statement
+			preparedStmt.execute();
+			output = "Inserted successfully";
+		} catch (Exception e) {
+			output = "Error while inserting";
+			System.err.println(e.getMessage());
+		}
+		return output;
+	}
+
+	
+	//method to read database
+	public String readDetails() {
 		String output = "";
 		try {
 			Connection con = connect();
@@ -29,8 +63,8 @@ public class Appointment {
 				return "Error while connecting to the database for reading.";
 			}
 			// Prepare the html table to be displayed
-			output = "<table border=\"1\"><tr><th>Appointment ID</th>" + "<th>Patient ID</th><th>Appointment Specification</th>"
-					+ "<th>Doctor's Name</th>" + "<th>Hospital Name</th><th>Date</th>  \"<th>Time</th><th>Status</th></tr>";
+			output = "<table border=\"1\"><tr><th>Appointment ID</th>" + "<th>Patient ID</th>" + "<th>Appointment Specification</th>"
+					+ "<th>Doctor's Name</th>" + "<th>Hospital Name</th>" + "<th>Date</th>" + "<th>Time</th>" + "<th>Status</th></tr>";
 			String query = "select * from appointment_doctor";
 			Statement stmt = con.createStatement();
 			ResultSet rs = stmt.executeQuery(query);
@@ -39,10 +73,10 @@ public class Appointment {
 				String AppointmentID = Integer.toString(rs.getInt("appointmentId"));
 				String PatientID = Integer.toString(rs.getInt("patientId"));
 				String AppointmentSpecification = rs.getString("appointmentSpec");
-				String DoctorName = rs.getString(rs.getString("doctorName"));
-				String HospitalName = rs.getString(rs.getString("hospitalName"));
-				Date DueDate =rs.getDate("dueDate");
-				Time DueTime =rs.getTime("dueTime");
+				String DoctorName = rs.getString("doctorName");
+				String HospitalName = rs.getString("hospitalName");
+				String DueDate =rs.getString("dueDate");
+				String DueTime =rs.getString("dueTime");
 				String Status = rs.getString("status");
 				
 				// Add into the html table
@@ -64,11 +98,67 @@ public class Appointment {
 			// Complete the html table
 			output += "</table>";
 		} catch (Exception e) {
-			output = "Error while reading the items.";
+			output = "Error while reading the details.";
 			System.err.println(e.getMessage());
 		}
 		return output;
 	}
 
+	//method to update details
+	public String updateDetails(String AppointmentID, String PatientID, String AppointmentSpecification, String DoctorName, String HospitalName, String DueDate, String DueTime, String Status)
+	 {
+	 String output = "";
+	 try
+	 {
+	 Connection con = connect();
+	 if (con == null)
+	 {return "Error while connecting to the database for updating."; }
+	 // create a prepared statement
+	 String query = "UPDATE appointment_doctor SET patientId=?,appointmentSpec=?,doctorName=?,hospitalName=?,dueDate=?,dueTime=?,status=?WHERE appointmentId=?"; 
+	 PreparedStatement preparedStmt = con.prepareStatement(query);
+	 // binding values
+	 preparedStmt.setInt(1, Integer.parseInt(PatientID));
+	 preparedStmt.setString(2, AppointmentSpecification);
+	 preparedStmt.setString(3, DoctorName);
+	 preparedStmt.setString(4, HospitalName);
+	 preparedStmt.setDate(5, Date.valueOf(DueDate));
+	 preparedStmt.setTime(6, Time.valueOf(DueTime));
+	 preparedStmt.setString(7, Status);
+	 preparedStmt.setInt(8, Integer.parseInt(AppointmentID));
+	 // execute the statement
+	 preparedStmt.execute();
+	 con.close();
+	 output = "Updated successfully";
+	 }
+	 catch (Exception e)
+	 {
+	 output = "Error while updating the details.";
+	 System.err.println(e.getMessage());
+	 }
+	 return output;
+	 }
 
+	//method to delete details
+	public String deleteDetails(String AppointmentID) {
+		String output = "";
+		try {
+			Connection con = connect();
+			if (con == null) {
+				return "Error while connecting to the database for deleting.";
+			}
+			// create a prepared statement
+			String query = "delete from appointment_doctor where AppointmentID=?";
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+			// binding values
+			preparedStmt.setInt(1, Integer.parseInt(AppointmentID));
+			// execute the statement
+			preparedStmt.execute();
+			con.close();
+			output = "Deleted successfully";
+		} catch (Exception e) {
+			output = "Error while deleting the details.";
+			System.err.println(e.getMessage());
+		}
+		return output;
+	}
 }
