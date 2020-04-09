c656e588021e0d881bb8ea77ea31fc56ee133883
Prabha97 <prabhashwari.lk@gmail.com>
2020-04-09 14:54:04 +0530

    created departments inserting method inside the model class

--- Department_Service/src/model/Department.java
+++ Department_Service/src/model/Department.java
@@ -2,6 +2,7 @@ package model;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
 public class Department {
 	//A common method to connect to the DB 
@@ -23,4 +24,40 @@ public class Department {
 				
 		return con; 
 		}
+	
+	public String insertDepartments(String hos_id, String dep_name, String head, String staff_vacon) {
+		String output = "";
+
+		try {
+			Connection con = connect();
+
+			if (con == null) {
+				return "Error while connecting to the database";
+			}
+
+			// create a prepared statement   
+			String query = " insert into departments (`Department_ID`,`Hospital_ID`,`Department_Name`,`Head`,`Staff_Vacancies`)"+" values (?, ?, ?, ?, ?)";
+
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+
+			// binding values 
+			preparedStmt.setInt(1, 0);   
+			preparedStmt.setInt(2, Integer.parseInt(hos_id));
+			preparedStmt.setString(3, dep_name);    
+			preparedStmt.setString(4, head);
+			preparedStmt.setInt(5, Integer.parseInt(staff_vacon));  
+
+			//execute the statement   
+			preparedStmt.execute();   
+			con.close(); 
+
+			output = "Inserted successfully";
+		}
+		catch (Exception e) {   
+			output = "Error while inserting Departments to the hospitals.";   
+			System.err.println(e.getMessage());  
+		} 
+
+		 return output; 
+	}
 }
