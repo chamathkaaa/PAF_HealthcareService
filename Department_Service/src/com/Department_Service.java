79c327cf30ee94a0198ff294079902e16de4fdcd
Prabha97 <prabhashwari.lk@gmail.com>
2020-04-09 15:00:50 +0530

    inside the department com class created method to read an example test message

--- Department_Service/src/com/Department_Service.java
+++ Department_Service/src/com/Department_Service.java
@@ -1,5 +1,27 @@
 package com;
 
-public class Department_Service {
+import model.Department;
 
+//For REST Service 
+import javax.ws.rs.*; 
+import javax.ws.rs.core.MediaType; 
+
+//For JSON 
+import com.google.gson.*; 
+
+//For XML 
+import org.jsoup.*; 
+import org.jsoup.parser.*; 
+import org.jsoup.nodes.Document; 
+
+@Path("/Department") 
+public class Department_Service {  
+	Department departmentObj = new Department();
+	
+	@GET  
+	@Path("/")  
+	@Produces(MediaType.TEXT_HTML)  
+	public String readDepartments()  {   
+		return "Prabha";
+	}
 }
