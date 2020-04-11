7310f0aa4546b49c33da77b2d8aa673d360ae4f4
Prabha97 <prabhashwari.lk@gmail.com>
2020-04-11 14:10:30 +0530

    Completed the implementation of delete operation

--- Department_Service/src/com/Department_Service.java
+++ Department_Service/src/com/Department_Service.java
@@ -67,11 +67,11 @@ public class Department_Service {
 		Document doc = Jsoup.parse(DepartmentsData, "", Parser.xmlParser());     
 		
 		//Read the value from the elements  
-		//String Department_ID = doc.select("Department_ID").text();
-		//String Hospital_ID = doc.select("Hospital_ID").text();
+		String Department_ID = doc.select("Department_ID").text();
+		String Hospital_ID = doc.select("Hospital_ID").text();
 		 
-		// String output = departmentObj.deleteDepartments(Department_ID,Hospital_ID); 
+		String output = departmentObj.deleteDepartments(Department_ID,Hospital_ID); 
 		 
-		 return output; 
-		 }
+		return output; 
+		}
 }
